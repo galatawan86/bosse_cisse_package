9ad5ba7082e0cb5626b9cd37c1dfe457b9eaf64e
galatawan86 <galatawan@gmail.com>
2020-03-03 00:51:53 +0100

    new info

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name='bosse_cisse_package',
+    name='package_idsi',
     version='1.0.0',
     author='my_email@email.com',
     description='Wine quality predictor - a packaged machine learning algorithm to predict wine quality',
